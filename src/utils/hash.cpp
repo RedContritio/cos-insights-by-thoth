20aef25c51da210c7a97f7506b1fcfb9e0a17543
RedContritio <RedContritio@qq.com>
2024-11-19 12:54:44 +0800

    fix warnings and unnecessary bin output

--- src/utils/hash.cpp
+++ src/utils/hash.cpp
@@ -4,10 +4,7 @@ using std::string;
 
 string calculateHash(const string& text) {
     unsigned char hash[SHA256_DIGEST_LENGTH];
-    SHA256_CTX sha256;
-    SHA256_Init(&sha256);
-    SHA256_Update(&sha256, text.c_str(), text.length());
-    SHA256_Final(hash, &sha256);
+    SHA256(reinterpret_cast<const unsigned char *>(text.c_str()), text.length(), hash);
 
     char buf[2 * SHA256_DIGEST_LENGTH + 1];
     for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
